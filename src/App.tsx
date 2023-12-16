459d0d7744a92c142f06f09919b4f5c4a9182537
Zul Ikram Musaddik Rayat <2010050@me.buet.ac.bd>
2023-12-16 12:33:50 +0600

    initial showcasing - showing cards

--- src/App.tsx
+++ src/App.tsx
@@ -13,9 +13,6 @@ import ProjectilePage from "./pages/projectile/page";
 import RainManPage from "./pages/rain/page";
 import VectorPage from "./pages/vector/page";
 import { ShapesOfInertia } from "./services/Moment_of_inertia";
-import DynamicsPage from "./pages/motion/page";
-import ElectricForcePage from "./pages/electric-force/page";
-import PendulumPage from "./pages/pendulum/page";
 
 function App() {
   return (
@@ -30,40 +27,42 @@ function App() {
           <Route path="dynamics" element={<DynamicsPage />} />
           <Route path="electric-force" element={<ElectricForcePage />} />
           <Route path="pendulum" element={<PendulumPage />} />
-          <Route path="moment-of-inertia" element={<Moment_of_inertia />} />
-          <Route
-            path="moment-of-inertia/point_mass"
-            element={<MOI_Basic shape={ShapesOfInertia.PointMass} />}
-          />
-          <Route
-            path="moment-of-inertia/two_point_mass"
-            element={<MOI_Basic shape={ShapesOfInertia.TwoMass} />}
-          />
-          <Route
-            path="moment-of-inertia/rod"
-            element={<MOI_DifferentAxes shape={ShapesOfInertia.Rod} />}
-          />
-          <Route
-            path="moment-of-inertia/rectangular_plate"
-            element={<MOI_DifferentAxes shape={ShapesOfInertia.Plate} />}
-          />
-          <Route
-            path="moment-of-inertia/solid_cuboid"
-            element={<MOI_DifferentAxes shape={ShapesOfInertia.Cuboid} />}
-          />
-          <Route
-            path="moment-of-inertia/cylinder"
-            element={<MOI_Cases shape={ShapesOfInertia.Cylinder} />}
-          />
+          <Route path="moment-of-inertia">
+            <Route index element={<Moment_of_inertia />} />
+            <Route
+              path="point_mass"
+              element={<MOI_Basic shape={ShapesOfInertia.PointMass} />}
+            />
+            <Route
+              path="two_point_mass"
+              element={<MOI_Basic shape={ShapesOfInertia.TwoMass} />}
+            />
+            <Route
+              path="rod"
+              element={<MOI_DifferentAxes shape={ShapesOfInertia.Rod} />}
+            />
+            <Route
+              path="rectangular_plate"
+              element={<MOI_DifferentAxes shape={ShapesOfInertia.Plate} />}
+            />
+            <Route
+              path="solid_cuboid"
+              element={<MOI_DifferentAxes shape={ShapesOfInertia.Cuboid} />}
+            />
+            <Route
+              path="cylinder"
+              element={<MOI_Cases shape={ShapesOfInertia.Cylinder} />}
+            />
 
-          <Route
-            path="moment-of-inertia/sphere"
-            element={<MOI_Cases shape={ShapesOfInertia.Sphere} />}
-          />
-          <Route
-            path="moment-of-inertia/disk"
-            element={<MOI_Cases shape={ShapesOfInertia.Disk} />}
-          />
+            <Route
+              path="sphere"
+              element={<MOI_Cases shape={ShapesOfInertia.Sphere} />}
+            />
+            <Route
+              path="disk"
+              element={<MOI_Cases shape={ShapesOfInertia.Disk} />}
+            />
+          </Route>
         </Route>
       </Routes>
     </HashRouter>
--- src/pages/page.tsx
+++ src/pages/page.tsx
@@ -1,5 +1,23 @@
-import { Link } from "react-router-dom";
+import { Link, To } from "react-router-dom";
 import projectile from "~/assets/cards/projectile.png";
+import rain from "~/assets/cards/rain.jpg";
+import vector from "~/assets/cards/vector.jpg";
+import boat from "~/assets/cards/boat.jpg";
+import dynamics from "~/assets/cards/dynamics.webp";
+import electric from "~/assets/cards/electric.png";
+import pendulum from "~/assets/cards/pendulum_card.jpg";
+import inertia from "~/assets/cards/inertia.jpg";
+
+const cards = [
+  { to: "projectile", img: projectile, title: "Projectile" },
+  { to: "rain", img: rain, title: "Rain" },
+  { to: "vector", img: vector, title: "Vector" },
+  { to: "boat-river", img: boat, title: "Boat River" },
+  { to: "dynamics", img: dynamics, title: "Dynamics" },
+  { to: "electric-force", img: electric, title: "Electric Force" },
+  { to: "pendulum", img: pendulum, title: "Pendulum" },
+  { to: "moment-of-inertia", img: inertia, title: "Moment of Inertia" },
+];
 
 export default function HomePage() {
   return (
@@ -7,21 +25,29 @@ export default function HomePage() {
       <section className="mt-6 mx-20 p-3">
         <h1 className="text-center text-3xl font-bold">Physics Calculator</h1>
 
-        <div className="mt-10 grid grid-cols-3">
-          <Link to="projectile">
-            <figure>
-              <img
-                src={projectile}
-                alt="projectile"
-                className="aspect-video object-fill rounded-md shadow-md hover:shadow-lg transition ease-in-out"
-              />
-              <figcaption className="text-xl font-semibold text-center mt-2">
-                Projectile
-              </figcaption>
-            </figure>
-          </Link>
+        <div className="mt-10 grid grid-cols-3 gap-x-6 gap-y-10">
+          {cards.map((card) => (
+            <ProjectCard key={card.title} {...card} />
+          ))}
         </div>
       </section>
     </main>
+  );
+}
+
+function ProjectCard(props: { to: To; img: string; title: string }) {
+  return (
+    <Link to={props.to} className="group">
+      <figure>
+        <img
+          src={props.img}
+          alt="projectile"
+          className="aspect-video object-fill rounded-md shadow-md hover:shadow-lg transition ease-in-out"
+        />
+        <figcaption className="text-xl font-semibold text-center mt-2 group-hover:underline">
+          {props.title}
+        </figcaption>
+      </figure>
+    </Link>
   );
 }
