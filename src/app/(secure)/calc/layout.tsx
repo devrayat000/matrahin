b3cba96a99e01e14abe21667f9e51eee8411df5c
Zul Ikram Musaddik Rayat <2010050@me.buet.ac.bd>
2024-01-03 22:44:41 +0600

    refactored login page

--- src/app/(secure)/calc/layout.tsx
+++ src/app/(secure)/calc/layout.tsx
@@ -1,30 +1,9 @@
-"use client";
-
-import { MathJaxContext } from "better-react-mathjax";
-
-const config = {
-  loader: { load: ["[tex]/html"] },
-  tex: {
-    packages: { "[+]": ["html"] },
-    inlineMath: [
-      ["$", "$"],
-      ["\\(", "\\)"],
-    ],
-    displayMath: [
-      ["$$", "$$"],
-      ["\\[", "\\]"],
-    ],
-  },
-};
+import CalcContext from "./Context";
 
 export default function CalcLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <MathJaxContext version={3} config={config}>
-      {children}
-    </MathJaxContext>
-  );
+  return <CalcContext>{children}</CalcContext>;
 }
