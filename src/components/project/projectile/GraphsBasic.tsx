6dd392413511fe2e478078edc06cb93d9b74b67d
Md. Jakaria Hossain <mdjakaria442020@gmail.com>
2024-02-08 17:46:01 +0600

    add: graph in projectile completed

--- src/components/project/projectile/GraphsBasic.tsx
+++ src/components/project/projectile/GraphsBasic.tsx
@@ -1,3 +1,5 @@
+// @ts-nocheck
+
 import { useAtomValue } from "jotai";
 import { Chart } from "react-google-charts";
 import { pointsAtom } from "./AnimationHelper";
@@ -5,24 +7,62 @@ import { pointsAtom } from "./AnimationHelper";
 const GraphsBasic = () => {
   const resultPoints = useAtomValue(pointsAtom);
 
-  let data = [["time", "y"]];
-  data.push(...resultPoints.map((point) => [point.t, point.y]));
+  const graph = [
+    {
+      data: [["time", "y"], ...resultPoints.map((point) => [point.t, point.y])],
+      title: "Height vs Time",
+      hAxisTitle: "Time (t)",
+      vAxisTitle: "Height (m)",
+    },
+    {
+      data: [["x", "y"], ...resultPoints.map((point) => [point.x, point.y])],
+      title: "Height vs Distance",
+      hAxisTitle: "Distance (x)",
+      vAxisTitle: "Height (m)",
+    },
+    {
+      data: [
+        ["time", "v"],
+        ...resultPoints.map((point) => [
+          point.t,
+          Math.sqrt(point.vx ** 2 + point.vy ** 2),
+        ]),
+      ],
+      title: "Speed vs Time",
+      hAxisTitle: "Time (t)",
+      vAxisTitle: "Speed (m/s)",
+    },
+    {
+      data: [
+        ["theta", "v"],
+        ...resultPoints.map((point) => [
+          Math.atan2(point.vy, point.vx) * (180 / Math.PI),
+          Math.sqrt(point.vx ** 2 + point.vy ** 2),
+        ]),
+      ],
+      title: "Speed vs Angle",
+      hAxisTitle: "Angle (θ)",
+      vAxisTitle: "Speed (m/s)",
+    },
+    {
+      data: [
+        ["time", "vy"],
+        ...resultPoints.map((point) => [point.t, point.vy]),
+      ],
+      title: "Vy vs Time",
+      hAxisTitle: "Time (t)",
+      vAxisTitle: "Vy (m/s)",
+    },
+  ];
 
   const options = {
-    title: "Height vs Time ",
-    // titlePosition: "in",
     titleTextStyle: {
       fontSize: 25,
       bold: true,
       italic: false,
       color: "black",
     },
-    hAxis: {
-      title: "Time (t)",
-    },
-    vAxis: {
-      title: "Height (m)",
-    },
+
     series: {
       0: { curveType: "function" },
     },
@@ -32,25 +72,39 @@ const GraphsBasic = () => {
   };
 
   return (
-    <div className="flex flex-col w-5/6 md:flex-row m-2 p-2      items-center justify-between  ">
-      <div className=" md:w-1/2 ">
-        <Chart
-          chartType="LineChart"
-          // width=""
-          height={500}
-          data={data}
-          options={options}
-        />
-      </div>
-      <div className="md:w-1/2 ">
-        <Chart
-          chartType="LineChart"
-          // width="1000"
-          height={500}
-          data={data}
-          options={options}
-        />
-      </div>
+    <div className="flex flex-col flex-wrap w-full md:flex-row m-2 items-center justify-between  ">
+      {graph.map((g, i) => (
+        <div key={i} className="md:w-1/2">
+          <Chart
+            chartType="LineChart"
+            // width="1000"
+            height={500}
+            data={g.data}
+            options={{
+              ...options,
+              title: g.title,
+              titleTextStyle: {
+                italic: false,
+                bold: true,
+                fontSize: 20,
+                color: "black",
+              },
+              hAxis: {
+                title: g.hAxisTitle,
+              },
+              vAxis: {
+                title: g.vAxisTitle,
+                // titleTextStyle: {
+                //   italic: false,
+                //   bold: true,
+                //   fontSize: 20,
+                //   color: "black",
+                // },
+              },
+            }}
+          />
+        </div>
+      ))}
     </div>
   );
 };
