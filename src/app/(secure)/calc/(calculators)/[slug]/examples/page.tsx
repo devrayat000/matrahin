e08306ac595c754a9eaffd5c3d6816299070a538
Zul Ikram Musaddik Rayat <98980963+devrayat000@users.noreply.github.com>
2024-02-11 20:43:36 +0600

    modified examples design

--- src/app/(secure)/calc/(calculators)/[slug]/examples/page.tsx
+++ src/app/(secure)/calc/(calculators)/[slug]/examples/page.tsx
@@ -1,5 +1,11 @@
 import Image from "next/image";
 import { use } from "react";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "~/components/ui/accordion";
 
 import { getExamples } from "~/services/graphql/example";
 
@@ -14,20 +20,49 @@ export default function ExamplePage({
     <div className="container">
       <h1 className="text-5xl text-center font-bold">{product.name}</h1>
 
-      <div className="mt-2">
+      <div className="mt-2 container max-w-4xl mx-auto">
         <h4 className="text-3xl font-medium">Examples</h4>
-        <ul className="flex flex-col gap-y-3">
-          {product.examples.map((example) => (
-            <li key={example.id}>
-              <Image
-                src={example.question.src}
-                width={320}
-                height={480}
-                alt="Question"
-              />
-            </li>
-          ))}
-        </ul>
+        <Accordion
+          type="multiple"
+          className="mt-5 list-image-check-circle"
+          asChild
+        >
+          <ul>
+            {product.examples.map((example) => {
+              return (
+                <AccordionItem key={example.id} value={example.id} asChild>
+                  <li>
+                    <AccordionTrigger>
+                      <Image
+                        src={example.question.src}
+                        width={320 * 2}
+                        height={480 * 2}
+                        alt="Question"
+                      />
+                    </AccordionTrigger>
+                    <AccordionContent className="flex gap-1 items-start">
+                      {example.answers.map((answer) => {
+                        return (
+                          <article
+                            key={answer.src}
+                            className="p-1 bg-slate-300 w-fit"
+                          >
+                            <Image
+                              src={answer.src}
+                              width={320 * 2}
+                              height={480 * 2}
+                              alt="Question"
+                            />
+                          </article>
+                        );
+                      })}
+                    </AccordionContent>
+                  </li>
+                </AccordionItem>
+              );
+            })}
+          </ul>
+        </Accordion>
       </div>
     </div>
   );
