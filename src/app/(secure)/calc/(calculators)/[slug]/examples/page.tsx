5628796184aadb2c61acd2c0865e8cad3fd60373
Zul Ikram Musaddik Rayat <98980963+devrayat000@users.noreply.github.com>
2024-02-12 15:37:26 +0600

    modified examples design

--- src/app/(secure)/calc/(calculators)/[slug]/examples/page.tsx
+++ src/app/(secure)/calc/(calculators)/[slug]/examples/page.tsx
@@ -24,44 +24,43 @@ export default function ExamplePage({
         <h4 className="text-3xl font-medium">Examples</h4>
         <Accordion
           type="multiple"
-          className="mt-5 list-image-check-circle"
-          asChild
+          className="mt-5 list-image-check-circle flex flex-col gap-2"
         >
-          <ul>
-            {product.examples.map((example) => {
-              return (
-                <AccordionItem key={example.id} value={example.id} asChild>
-                  <li>
-                    <AccordionTrigger>
-                      <Image
-                        src={example.question.src}
-                        width={320 * 2}
-                        height={480 * 2}
-                        alt="Question"
-                      />
-                    </AccordionTrigger>
-                    <AccordionContent className="flex gap-1 items-start">
-                      {example.answers.map((answer) => {
-                        return (
-                          <article
-                            key={answer.src}
-                            className="p-1 bg-slate-300 w-fit"
-                          >
-                            <Image
-                              src={answer.src}
-                              width={320 * 2}
-                              height={480 * 2}
-                              alt="Question"
-                            />
-                          </article>
-                        );
-                      })}
-                    </AccordionContent>
-                  </li>
-                </AccordionItem>
-              );
-            })}
-          </ul>
+          {product.examples.map((example) => {
+            return (
+              <AccordionItem
+                key={example.id}
+                value={example.id}
+                className="px-8 rounded-xl border-2 border-border"
+              >
+                <AccordionTrigger>
+                  <Image
+                    src={example.question.src}
+                    width={320 * 2}
+                    height={480 * 2}
+                    alt="Question"
+                  />
+                </AccordionTrigger>
+                <AccordionContent className="flex gap-1 items-start">
+                  {example.answers.map((answer) => {
+                    return (
+                      <article
+                        key={answer.src}
+                        className="p-1 bg-slate-300 w-fit"
+                      >
+                        <Image
+                          src={answer.src}
+                          width={320 * 2}
+                          height={480 * 2}
+                          alt="Question"
+                        />
+                      </article>
+                    );
+                  })}
+                </AccordionContent>
+              </AccordionItem>
+            );
+          })}
         </Accordion>
       </div>
     </div>
