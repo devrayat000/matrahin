c017d2eb796095e70c0cb89636447dbb5b923011
Md. Jakaria Hossain <mdjakaria442020@gmail.com>
2024-02-20 16:32:03 +0600

    Refactored Electric Force simulation

---
+++ src/app/(secure)/calc/(calculators)/electric-force-between-charges-physics/store.ts
@@ -0,0 +1,12 @@
+import { atom } from "jotai";
+
+export const scaleAtom = atom(10);
+
+export const dimensionsAtom = atom({
+  canvasWidth: 350,
+  canvasHeight: 350,
+  yOriginOffset: 175,
+});
+
+export const MAX_ZOOM = 20;
+export const MIN_ZOOM = 0.5;
