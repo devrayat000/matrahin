514ab80396a37ee971c2f363f5a7c5c734ba603a
Zul Ikram Musaddik Rayat <98980963+devrayat000@users.noreply.github.com>
2024-02-20 15:03:40 +0600

    global header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,11 @@ import "../index.css";
 import { Metadata } from "next";
 import { GoogleAnalytics } from "@next/third-parties/google";
 import logo from "~/assets/logo.png";
+import Link from "next/link";
+import Image from "next/image";
+import MainNav from "./(main)/MainNav";
+import UserNav from "./(main)/UserNav";
+import MainDrawer from "./(main)/MainDrawer";
 
 export const metadata: Metadata = {
   appleWebApp: { capable: true, title: "Matrahin" },
@@ -37,6 +42,20 @@ export default async function RootLayout({
       </head>
       <body>
         {/* <Session session={session}>{children}</Session> */}
+        <header className="border-b w-full sticky top-0 z-50 bg-background/80 backdrop-blur-md">
+          <div className="flex h-16 justify-between items-center px-8">
+            <Link href="/" className="inline-flex items-center gap-2">
+              <Image src={logo} alt="Matrahin" width={44} height={44} />
+            </Link>
+            <div className="hidden sm:flex">
+              <MainNav className="mx-6" />
+            </div>
+            <div className="hidden sm:flex items-center">
+              <UserNav />
+            </div>
+            <MainDrawer />
+          </div>
+        </header>
         {children}
         <GoogleAnalytics gaId={process.env.GOOGLE_TAG} />
       </body>
