2a53fb0ed69880693d97cb591fded899b5594033
Zul Ikram Musaddik Rayat <98980963+devrayat000@users.noreply.github.com>
2024-02-20 15:03:40 +0600

    global header

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -14,20 +14,6 @@ export default async function MainLayout({
 }) {
   return (
     <div>
-      <header className="border-b w-full sticky top-0 z-50 bg-background/80 backdrop-blur-md">
-        <div className="flex h-16 justify-between items-center px-8">
-          <Link href="/" className="inline-flex items-center gap-2">
-            <Image src={logo} alt="Matrahin" width={44} height={44} />
-          </Link>
-          <div className="hidden sm:flex">
-            <MainNav className="mx-6" />
-          </div>
-          <div className="hidden sm:flex items-center">
-            <UserNav />
-          </div>
-          <MainDrawer />
-        </div>
-      </header>
       <main className="min-h-[calc(100vh-10rem)]">{children}</main>
       <Footer />
     </div>
