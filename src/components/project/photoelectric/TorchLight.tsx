4cc0e753c981aa9008980c6abd489440c507291d
Md. Jakaria Hossain <mdjakaria442020@gmail.com>
2024-06-12 22:47:51 +0600

    moved files to secured route

--- src/components/project/photoelectric/TorchLight.tsx
+++ src/components/project/photoelectric/TorchLight.tsx
@@ -10,11 +10,12 @@ const TorchLight = ({ color }: { color: string }) => {
       <line x1="180" x2="240" y1="300" y2="280" />
 
       <path d="M 240 220 Q 255 250 240 280" stroke="black" fill="transparent" />
-      <line x1="240" x2="400" y1="220" y2="220" />
-      <line x1="240" x2="400" y1="280" y2="280" />
+      <line x1="240" x2="365" y1="220" y2="220" />
+      <line x1="240" x2="365" y1="280" y2="280" />
 
-      <path d="M 400 220 Q 435 250 400 280" stroke="black" fill="transparent" />
-      <path d="M 400 220 Q 365 250 400 280" stroke="black" fill="transparent" />
+      {/* <path d="M 400 220 Q 435 250 400 280" stroke="black" fill="transparent" /> */}
+      <path d="M 365 220 Q 380 250 365 280" stroke="black" fill="transparent" />
+      {/* <path d="M 400 220 Q 365 250 400 280" stroke="black" fill="transparent" /> */}
     </g>
   );
 };
