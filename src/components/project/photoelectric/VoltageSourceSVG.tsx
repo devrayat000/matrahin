e6dd02d46ea9087adcb52060e63b16131dab745c
Md. Jakaria Hossain <mdjakaria442020@gmail.com>
2024-08-18 18:32:23 +0600

    FIX: voltage source direction in photoelectric

--- src/components/project/photoelectric/VoltageSourceSVG.tsx
+++ src/components/project/photoelectric/VoltageSourceSVG.tsx
@@ -12,16 +12,19 @@ const VoltageSource = ({
       viewBox="0 0 100 100"
       xmlns="http://www.w3.org/2000/svg"
       strokeWidth={4}
-      transform={
-        plusLeft ? "translate(10 0)" : "translate(30 0) rotate(180 35 35)"
-      }
     >
-      <Line num={[10, 30, 35, 35]} />
-      <Line num={[30, 30, 10, 60]} />
-      <Line num={[40, 40, 25, 45]} />
-      <Line num={[50, 50, 10, 60]} />
-      <Line num={[60, 60, 25, 45]} />
-      <Line num={[60, 85, 35, 35]} />
+      <g
+        transform={
+          plusLeft ? "translate(10 0)" : "translate(30 0) rotate(180 35 35)"
+        }
+      >
+        <Line num={[10, 30, 35, 35]} />
+        <Line num={[30, 30, 10, 60]} />
+        <Line num={[40, 40, 25, 45]} />
+        <Line num={[50, 50, 10, 60]} />
+        <Line num={[60, 60, 25, 45]} />
+        <Line num={[60, 85, 35, 35]} />
+      </g>
     </svg>
   );
 };
